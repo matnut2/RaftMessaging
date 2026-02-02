334c7395944f9f7da921083a924d255f1cac4392
Matteo S. <matteosolda12@gmail.com>
2026-02-02 20:41:14 +0100

    Network simulator

---
+++ src/main/java/com/raft/core/Network.java
@@ -0,0 +1,28 @@
+package com.raft.core;
+
+import com.raft.rpc.AppendEntriesRequest;
+import com.raft.rpc.AppendEntriesResponse;
+import com.raft.rpc.RequestVoteRequest;
+import com.raft.rpc.RequestVoteResponse;
+
+import java.util.concurrent.CompletableFuture;
+
+public interface Network {
+    /**
+     * Sends a RequestVote RPC to a specific target node.
+     * @param targetNodeId The ID of the receiving node.
+     * @param request The RPC payload.
+     * @return A Future containing the response.
+     */
+
+        CompletableFuture<RequestVoteResponse> sendRequestVote(String targetNodeID, RequestVoteRequest request);
+    /**
+     * Sends an AppendEntries RPC (log replication or heartbeat).
+     * @param targetNodeId The ID of the receiving node.
+     * @param request The RPC payload.
+     * @return A Future containing the response.
+     */
+
+    CompletableFuture<AppendEntriesResponse> sendAppendEntries(String targetNodeID, AppendEntriesRequest request);
+
+    } 
